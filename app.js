7280e4849f31693fcf77833bc94b8e7dab6a600e
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-18 16:44:01 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//CRUD
+
+const express = required('express');
+const app = express();
+const ConnectDB = require('./db/dbConnect')
+
+
+app.use(express.static('./public'))
+app.use(express.json())
+
+// routes
+// app.use("/api/v1/tasks", tasks)
+
+const port = 5000
+
+const server =  async () => {
+    try{
+        await ConnectDB(url)
+        app.listen(port, () => console.log(`Server listening on port ${port}`))
+    } catch(err){
+        console.log(err)
+    }
+}
+
+server()
+
+// MongoDb
+// Postgresql
+// postman
